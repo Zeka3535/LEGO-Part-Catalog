5d245aaa5a9d449de5cbe31180b71de209707bf1
Zeka3535 <50769374+Zeka3535@users.noreply.github.com>
2025-09-20 17:08:12 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lego-catalog-cache-v28';
+const CACHE_NAME = 'lego-catalog-cache-v29';
 
 // Keep precache minimal to avoid install failures due to missing files
 const PRECACHE_ASSETS = [
