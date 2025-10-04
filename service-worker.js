2da13175579d6a66ea4f829b0559498102778379
Zeka3535 <50769374+Zeka3535@users.noreply.github.com>
2025-10-04 20:10:50 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lego-catalog-cache-v37';
+const CACHE_NAME = 'lego-catalog-cache-v38';
 
 // Keep precache minimal to avoid install failures due to missing files
 const PRECACHE_ASSETS = [
